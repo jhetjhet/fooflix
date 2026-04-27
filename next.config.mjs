33b8769b8d5f25fd9930eae27995af601d2e3a52
Austin Mallari <villasanamallariaustin@gmail.com>
2026-04-27 16:35:03 +0800

    docker standalone config
    source from https://github.com/vercel/next.js/blob/canary/examples/with-docker/Dockerfile

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // Helps catch bugs during development
+  output: "standalone", // Optimizes the build for deployment
   /* Only include the below if you absolutely need them */
   // typescript: { ignoreBuildErrors: false }, 
   // images: { unoptimized: false },
